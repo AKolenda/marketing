59d239385ee295be55d72c325fd6cedfe4af2f90
AKolenda <akole779@mtroyal.ca>
2023-11-28 01:16:28 -0700

    done?

--- contact.js
+++ contact.js
@@ -1,19 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
     var form = document.getElementById('contact-form');
-    var thankYouMessage = document.getElementById('thank-you-message');
+    var submitButton = form.querySelector('button[type="submit"]');
 
     form.addEventListener('submit', function(event) {
         event.preventDefault(); // Prevents form from submitting to a server
+        form.reset(); // Clear the form fields
 
-        // Clear the form fields
-        form.reset();
+        // Create and show toast notification
+        var toast = document.createElement('div');
+        toast.className = 'form-toast';
+        toast.textContent = 'Thank You For Submitting Your Request!';
+        document.body.appendChild(toast);
 
-        // Show thank you message
-        thankYouMessage.style.display = 'block';
-
-        // Optionally hide the message after some time
-        setTimeout(function() {
-            thankYouMessage.style.display = 'none';
-        }, 3000); // Hide after 3 seconds
+        // Animate toast notification
+        setTimeout(() => {
+            toast.classList.add('show');
+            setTimeout(() => {
+                toast.classList.remove('show');
+                setTimeout(() => {
+                    document.body.removeChild(toast);
+                }, 500); // Wait for animation to finish before removing
+            }, 3000); // Duration the toast notification stays on screen
+        }, 100);
     });
 });
